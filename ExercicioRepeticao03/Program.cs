d522674f87887883b56842d543d8021218c21dcf
luiz.caetano <luiz.caetano@MMQ003477>
2023-08-23 14:55:37 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioRepeticao03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace curso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado);
+            }
+
+        }
+
+    }
+
+}
